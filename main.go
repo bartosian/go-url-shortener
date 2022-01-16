02c5e240c3281cdf9301d1da9f8ec4a4be4937ff
Kiryl Bartoshevich <bartosian1989@gmail.com>
2022-01-16 14:03:43 -0500

    Added simple file reader example

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
-	"strings"
 )
 
 // func main() {
@@ -25,33 +25,54 @@ import (
 // 	}
 // }
 
-var nrchars, nrwords, nrlines int
+// var nrchars, nrwords, nrlines int
+
+// func main() {
+// 	nrchars, nrlines, nrwords = 0, 0, 0
+// 	inputReader := bufio.NewReader(os.Stdin)
+// 	fmt.Println("Please provide your text: ")
+
+// 	for {
+// 		input, err := inputReader.ReadString('\n')
+// 		if err != nil {
+// 			fmt.Println("There were errors reading text!")
+// 			return
+// 		}
+
+// 		if input == "S\n" {
+// 			fmt.Println("These are your counts: ")
+// 			fmt.Println("Number of chars: %d\n", nrchars)
+// 			fmt.Println("Number of words: %d\n", nrwords)
+// 			fmt.Println("Number of lines: %d\n", nrlines)
+// 			os.Exit(0)
+// 		}
+// 		Counters(input)
+// 	}
+// }
+
+// func Counters(input string) {
+// 	nrchars += len(input) - 1
+// 	nrwords += len(strings.Fields(input))
+// 	nrlines++
+// }
 
 func main() {
-	nrchars, nrlines, nrwords = 0, 0, 0
-	inputReader := bufio.NewReader(os.Stdin)
-	fmt.Println("Please provide your text: ")
+	inputFile, inputError := os.Open("input.dat")
+	if inputError != nil {
+		fmt.Printf("An error occurred on opening the inputfile.\n" +
+			"Try open it and refactor.\n")
+		return
+	}
+
+	defer inputFile.Close()
+	inputReader := bufio.NewReader(inputFile)
 
 	for {
-		input, err := inputReader.ReadString('\n')
-		if err != nil {
-			fmt.Println("There were errors reading text!")
+		inputString, readerError := inputReader.ReadString('\n')
+		if readerError == io.EOF {
 			return
 		}
 
-		if input == "S\n" {
-			fmt.Println("These are your counts: ")
-			fmt.Println("Number of chars: %d\n", nrchars)
-			fmt.Println("Number of words: %d\n", nrwords)
-			fmt.Println("Number of lines: %d\n", nrlines)
-			os.Exit(0)
-		}
-		Counters(input)
+		fmt.Printf("The input was: %s", inputString)
 	}
-}
-
-func Counters(input string) {
-	nrchars += len(input) - 1
-	nrwords += len(strings.Fields(input))
-	nrlines++
 }
